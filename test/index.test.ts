f33ff18e0a0ad0118a0b4889cdbde673c61ac8b0
luke358 <1494135711@qq.com>
2023-09-26 10:02:54 +0800

    test: 添加测试用例

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { compareVersion } from '../src'
+import { compareVersion, getMaxVersion } from '../src'
 
 describe('should', () => {
   it('exported', () => {
@@ -14,5 +14,15 @@ describe('compareVersion', () => {
 
   it('exported version', () => {
     expect(compareVersion('1.2.1', '1.2.0')).toMatchInlineSnapshot('1')
+  })
+})
+
+describe('maxVersion', () => {
+  it('exported pre', () => {
+    expect(getMaxVersion('1.3.2-alpha', '1.3.2-beta')).toMatchInlineSnapshot('"1.3.2-beta"')
+  })
+
+  it('exported version', () => {
+    expect(getMaxVersion('1.2.1', '1.2.0')).toMatchInlineSnapshot('"1.2.1"')
   })
 })
